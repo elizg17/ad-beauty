c51f1d6734a1a0017af87cc1d60171cb8be51f15
Elizabeth Griffith <lizzylizard00@elizabeth-griffiths-macbook-pro.local>
2016-01-30 23:35:16 -0800

    contact.php

---
+++ contactme.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>AngelaD Beauty-Seattle Area Makeup Artist-Contact Me</title>
+    <meta name="robots" content="noindex,nofollow" />
+    <meta name="viewport" content="width=device-width, maximum-scale=2" />
+	<link href='http://fonts.googleapis.com/css?family=Lusitana:700' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="css/ad.css" />
+   
+    <!--[if ltIE9]>
+       <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+     <![endif]-->
+</head>
+
+<body>
+
+<div id="navcontainer">
+<img class="logo"  src="images/adlogo.gif" style="width:230px; height: 98px;" alt="logo" />
+<nav class="nav">
+	<ul>
+	<li><a href="adbeuty_1.html" title="home page" class="current">Main Page</a></li>
+	<li><a href="portfolio.html" title="portfolio of work">Portfolio</a> </li>
+	<li><a href="services.html" title="menu of services">Services</a></li>
+	<li><a href="about.html" title="about angela">About Angela</a> </li>
+	<li class="current"><a href="contactme.php" title="contact angela d">Contact Me</a></li>
+	</ul>
+	</nav>
+
+<div class="wrapper">
+	<header>
+	<h3>Contact Angela</h3>
+	</header>
+	
+	<?php
+		include'contact.php';
+	?>
+	
+	
+<!-- START Footer --> <div id="footer"><img class="socialmedia" src=" http://edison.seattlecentral.edu/~egriff04/finalproject/images/socialmedia.gif"  
+  style="width: 260px; height:36px;" alt="social media logo" > <br><small>&copy; 2013, All Rights Reserved &#8226; <a
+   href="http://validator.w3.org/check/referer" target="_blank">Valid
+   HTML</a> &#8226; <a
+   href="http://jigsaw.w3.org/css-validator/check?uri=referer"
+   target="_blank">Valid CSS</a> &#8226; <a href="thewportraiture.com"
+   target="_blank"> W Portraiture </a> &#8226; <a href="yuliadygert.com"
+   target="_blank"> Yulia Dygert </a> &#8226;<a href="disclaimer.html" target="_blank"> Disclaimer </a> 
+   &#8226; Website by Beth Grifith </small> <!-- END Footer --> 
+    </div>
+ </div>
+</body>
+</html>
